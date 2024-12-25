ef173e0d95f15fcfb6f5dcdeef8ef80f378cc61f
kanzariyasunil <kanzariyasunil2022004@gmail.com>
2024-12-25 14:15:22 +0530

    commit

--- main.py
+++ main.py
@@ -1,62 +1,31 @@
-from flask import Flask,request,json
+from flask import Flask
+
 
 app = Flask(__name__)
 
 
-
-@app.route("/",methods = ["GET"])
+@app.route('/')
 def home():
-    return "Hiii"
-
-# @app.route("/add",methods = ["POST"])
-# def add():
-#     roll = request.form.get("roll")
-#     name = request.form.get("name")
-#     bike = request.form.get("bike")
-
-#     if roll and name and bike:
-#         querys = "INSERT INTO public.crud(roll, name, bike) VALUES (%(roll)s, %(name)s, %(bike)s);"
-#         make_db_call(query=querys,parameter={
-#             "roll" : roll,
-#             "name" : name,
-#             "bike" : bike
-#         })
-#     return "data will be added"
-
-# @app.route("/update",methods = ["PUT"])
-# def update():
-#     name = request.form.get("name")
-#     bike = request.form.get("bike")
-#     roll = request.form.get("roll")
-#     querys = ""
-
-#     if name and bike:
-#         querys = "update public.crud set name = %(name)s,bike = %(bike)s where roll = %(roll)s"
-#     elif name:
-#         querys = "update public.crud set name = %(name)s where roll = %(roll)s"
-#     else:
-#         querys = "update public.crud set bike = %(bike)s where roll = %(roll)s"
-    
-#     make_db_call(query=querys,parameter={
-#         "roll" : roll,
-#         "name" : name,
-#         "bike" : bike
-#     })
-
-#     return "update sussefully"
-
-# @app.route("/delete",methods = ["DELETE"])
-# def delete():
-#     querys = "delete from public.crud where roll = %(roll)s"
-#     try:
-#         make_db_call(query=querys,parameter={
-#         "roll":request.form.get("roll")
-#     })
-#     except:
-#         return {"message":"Please enter valid roll no"}
-
-#     return "data deleted"
+    return 'Home Page Route'
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/about')
+def about():
+    return 'About Page Route'
+
+
+@app.route('/portfolio')
+def portfolio():
+    return 'Portfolio Page Route'
+
+
+@app.route('/contact')
+def contact():
+    return 'Contact Page Route'
+
+
+@app.route('/api')
+def api():
+    with open('data.json', mode='r') as my_file:
+        text = my_file.read()
+        return text
