35c38a6f9dad6e64aeefc91c05c1e52a8fd32357
kanzariyasunil <kanzariyasunil2022004@gmail.com>
2024-12-25 14:29:13 +0530

    ok

--- app.py
+++ app.py
@@ -1,34 +1,11 @@
-from flask import Flask
-
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def home():
-    return 'Home Page Route'
+def index():
+    return render_template('index.html')
 
 
-@app.route('/about')
-def about():
-    return 'About Page Route'
-
-
-@app.route('/portfolio')
-def portfolio():
-    return 'Portfolio Page Route'
-
-
-@app.route('/contact')
-def contact():
-    return 'Contact Page Route'
-
-
-@app.route('/api')
-def api():
-    with open('data.json', mode='r') as my_file:
-        text = my_file.read()
-        return text
-    
 if __name__ == "__main__":
     app.run(debug=True)
