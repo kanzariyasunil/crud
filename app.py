b89333d292887905f79301e9cc91aa643f159ff2
kanzariyasunil <kanzariyasunil2022004@gmail.com>
2024-12-25 17:52:39 +0530

    app py added

--- app.py
+++ app.py
@@ -12,59 +12,56 @@ def index():
 
 @app.route("/information",methods = ["GET"])
 def information():
-    query = "select * from crud"
+    query = "select * from Persons"
     data = make_db_call(query=query,returns=True)
     try:
         response = [{"id":item[0],
                  "name":item[1],
-                 "bike":item[2]} for item in data]
+                 } for item in data]
     except Exception as e:
         return str(e)
     return response
 
 @app.route("/add",methods = ["POST"])
 def add():
-    roll = request.form.get("roll")
-    name = request.form.get("name")
-    bike = request.form.get("bike")
+    roll = request.form.get("PersonID")
+    name = request.form.get("FirstName")
 
-    if roll and name and bike:
-        querys = "INSERT INTO crud(roll, name, bike) VALUES (%(roll)s, %(name)s, %(bike)s);"
+    if roll and name :
+        querys = "INSERT INTO Persons(PersonID , FirstName) VALUES (%(PersonID)s, %(FirstName)s);"
         make_db_call(query=querys,parameters={
-            "roll" : roll,
-            "name" : name,
-            "bike" : bike
+            "PersonID" : roll,
+            "FirstName" : name,
+            
         })
     return "data will be added"
 
 @app.route("/update",methods = ["PUT"])
 def update():
-    name = request.form.get("name")
-    bike = request.form.get("bike")
-    roll = request.form.get("roll")
+    name = request.form.get("FirstName")
+    roll = request.form.get("PersonID")
     querys = ""
 
-    if name and bike:
-        querys = "update public.crud set name = %(name)s,bike = %(bike)s where roll = %(roll)s"
-    elif name:
-        querys = "update public.crud set name = %(name)s where roll = %(roll)s"
-    else:
-        querys = "update public.crud set bike = %(bike)s where roll = %(roll)s"
+    if name:
+        querys = "update public.Persons set FirstName = %(FirstName)s where PersonID = %(PersonID)s"
+    
     
     make_db_call(query=querys,parameters={
-        "roll" : roll,
-        "name" : name,
-        "bike" : bike
+        "PersonID" : roll,
+        "FirstName" : name,
+        
     })
 
     return "update sussefully"
 
 @app.route("/delete",methods = ["DELETE"])
 def delete():
-    querys = "delete from public.crud where roll = %(roll)s"
+    querys = "delete from public.Persons where PersonID = %(PersonID)s"
     try:
-        make_db_call(query=querys,parameters={
-        "roll":request.form.get("roll")
+        make_db_call(
+            query=querys,
+            parameters={
+            "PersonID":request.form.get("PersonID")
     })
     except:
         return {"message":"Please enter valid roll no"}
